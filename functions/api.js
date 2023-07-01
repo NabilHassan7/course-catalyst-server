72cb3cd9ce3018c1ab080d92d1209f69bafe1b18
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-07-01 23:54:41 +0600

    updated file structure

--- functions/api.js
+++ functions/api.js
@@ -1,17 +1,18 @@
 const express = require('express');
-
-const app = express;
-
+// running the API server using express
+const app = express();
+// initiating CORS 
 const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
+// calling CORS function
 app.use(cors());
 
 app.get('/',(req, res) => {
     res.send('API running');
 })
-
+// calling nodemon
 app.listen(port, () => {
     console.log('Base server is running at port 5000');
 })
